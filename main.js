bc8697b652aac371e3deaca22ce07d3b96a96190
Annya <annya@example.com>
2023-02-11 19:02:51 +0500

    ytytytherh

---
+++ main.js
@@ -0,0 +1,24 @@
+const fastify = require('fastify')({
+    logger: true
+  })
+ 
+  // Declare a route
+  fastify.get('/:firstNumber/:secondNumber', function (request, reply) {
+    console.log(request.params)
+    let a = request.params.firstNumber 
+    let b = request.params.secondNumber
+    let  c = Number(a)+Number(b)
+    reply.send({c})
+
+  })
+  
+
+
+  // Run the server!
+  fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    // Server is now listening on ${address}
+  })
